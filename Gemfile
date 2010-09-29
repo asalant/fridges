6854357ed5043c5014666c01bd5c874a281b437a
Alon Salant <alon@salant.org>
2010-09-28 22:07:58 -0700

    created empty rails 3 app

--- Gemfile
+++ Gemfile
@@ -3,14 +3,11 @@ source 'http://rubygems.org'
 gem 'heroku'
 gem 'rails', '3.0.0'
 gem 'haml', '>= 3.0.18'
-gem 'mysql2', '0.2.3'
+gem 'json'
+gem 'sqlite3'
 
-# Uncomment for local New Relic testing, as NR won't run with WebBrick
-# gem 'mongrel'
-
-group :test do
+group :test, :development do
   gem 'rspec-rails', '>= 2.0.0.beta.20'
   gem 'rspec_tag_matchers'
   gem 'mocha', '>= 0.9.8'
-  gem 'fakeweb', '>= 1.2.8'
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fridges::Application.config.secret_token = '011ba2918465f709bda68a7e8c8f77ffe1787bf7817dd6a4d799d7fb85b94554588715c9a19e93cab76066e827f47a87a11a6ed53d3581aff3670740283a03bf'
