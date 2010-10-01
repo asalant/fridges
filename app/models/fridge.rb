6d9908d3fb110326f7f0d0489431e32f7f4ae8f4
Alon Salant <alon@salant.org>
2010-10-01 00:20:54 -0700

    lets make photos 600 wide instead of 700

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,6 +1,6 @@
 class Fridge < ActiveRecord::Base
   has_attached_file :photo,
-    :styles         => {:large => "700", :thumb => "100x100#"},
+    :styles         => {:large => "600", :thumb => "100x100#"},
     :storage        => :s3,
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => "/:style/:filename"
