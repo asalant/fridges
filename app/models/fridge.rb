6e09333b2a5b97778269f968fa02afe85c65813b
Alon Salant <alon@salant.org>
2010-10-01 00:14:53 -0700

    don't commit S3 keys, use heroku config vars instead

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,4 +1,7 @@
 class Fridge < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :large => "700", :thumb => "100x100#" }
-
+  has_attached_file :photo,
+    :styles         => {:large => "700", :thumb => "100x100#"},
+    :storage        => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => "/:style/:filename"
 end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -33,19 +34,25 @@ module Fridges
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = [
       'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js',
-      'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js',
-      'jquery.rails']
+        'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js',
+        'jquery.rails']
 
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding                                     = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters                            += [:password]
 
     config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => true, :views => false
+    end
+
+    config.before_configuration do
+      # Load heroku vars from local file
+      heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+      load(heroku_env) if File.exists?(heroku_env)
     end
 
   end
