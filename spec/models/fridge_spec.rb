06553a45c32bbb02f87e6c5e8858505ca9222fb4
Alon Salant <alon@salant.org>
2010-10-08 00:40:13 -0700

    removed Fridge#to_param

--- spec/models/fridge_spec.rb
+++ spec/models/fridge_spec.rb
@@ -25,10 +25,4 @@ describe Fridge do
       @fridge.key.length.should == 6
     end
   end
-
-  context "for urls" do
-    it "should return key for to_param" do
-      fridges(:alon).to_param.should == fridges(:alon).key
-    end
-  end
 end
