2ab2459fd8856c7341c8a4df954d941d19f31cd7
Alon Salant <alon@salant.org>
2010-10-07 23:02:41 -0700

    try to create fridge from first attachement

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,13 @@
 class WebhooksController < ApplicationController
   def sendgrid
-    puts "Got incoming sendgrid email:\n#{params.inspect}"
+    Rails.logger.info "Got incoming sendgrid email:\n#{params.inspect}"
+
+    begin
+      Fridge.create!(:photo => params['attachment1'], :name => params['subject'], :description => params['description'])
+    rescue Exception => e
+      Rails.logger.error "Error creating fridge from sendgrid webhook: #{e.message}, #{e.backtrace}"
+    end
+
     render :status => :ok, :nothing =>  true
   end
 
