169aa912e5cb2cc783a21bc03611b31cc8cbaacb
Alon Salant <alon@salant.org>
2010-10-14 23:51:23 -0700

    nil check for profile url

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def profile_image_url(user)
-    return nil if user.nil?
+    return 'profile_placeholder.jpg' if user.nil?
     "http://graph.facebook.com/#{user.facebook_id}/picture"
   end
 end
