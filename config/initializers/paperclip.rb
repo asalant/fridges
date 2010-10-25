1c5123f3b169fdc2dd8315341c185834ae915820
Alon Salant <alon@salant.org>
2010-10-24 22:57:49 -0700

    added paperclip s3 timing

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,26 @@
+module Paperclip
+  module Storage
+    module S3
+      def create_bucket_with_timing
+        timer(:flush_writes) do
+          create_bucket_without_timing
+        end
+      end
+      alias_method_chain :create_bucket, :timing
+
+      def flush_writes_with_timing
+        timer(:flush_writes) do
+          flush_writes_without_timing
+        end
+      end
+      alias_method_chain :flush_writes, :timing
+
+      def timer(what)
+        start = Time.now
+        yield
+        Rails.logger.debug "#{what} executed in #{Time.now - start} sec"
+      end
+
+    end
+  end
+end
