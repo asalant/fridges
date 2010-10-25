5fe8ea05d8c6ab902c798ec0033544c44eb688d6
Alon Salant <alon@salant.org>
2010-10-24 21:11:49 -0700

    post formatting testing

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -82,11 +82,13 @@ class FridgesController < ApplicationController
   private
 
   def post_to_facebook(access_token, fridge)
+    # See 'Publishing' section of http://developers.facebook.com/docs/reference/api/post
+    # TODO: Needs tweaking to get formatting and content to look good
     post = {
       :message => "Check out my fridge!",
       :picture => fridge.photo.url(:large).gsub(%r(^/system/), "http://localhost:300/system/"), # for development
       :link    => fridge_key_url(fridge.key, :host => 'frdg.us'),
-      :description => "You are what you refrigerate. Don't you agree?<br><br>Post your own so I can see!"
+      :description => "You are what you refrigerate. Don't you agree?\n\nPost your own so I can see!"
       }
     Rails.logger.debug "Posting to Facebook: #{post.inspect}"
     graph = Koala::Facebook::GraphAPI.new(access_token)
