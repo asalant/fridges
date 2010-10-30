c316cffcc43b4aa07bae218d3f44cb7ac3fbcde7
Alon Salant <alon@salant.org>
2010-10-29 17:44:21 -0700

    add cache control headers for Rack::File assets

--- config.ru
+++ config.ru
@@ -1,4 +1,12 @@
 # This file is used by Rack-based servers to start the application.
 
-require ::File.expand_path('../config/environment',  __FILE__)
+# Expire static assets in one year, Rails will cache bust with new deployments
+require 'rack_cache_headers'
+use RackCacheHeaders, {
+  %r(^/(stylesheets)|(javascripts)|(images)/) => {:cache_control => "max-age=15768000, public", :expires => 15768000}
+}
+
+require ::File.expand_path('../config/environment', __FILE__)
 run Fridges::Application
+
+
---
+++ lib/rack_cache_headers.rb
@@ -0,0 +1,21 @@
+# See http://www.hokstad.com/rack-middleware-adding-cache-headers.html
+# Set cache control headers for Rack::File, configured in config.ru
+class RackCacheHeaders
+  def initialize(app, patterns)
+    @app = app
+    @patterns = patterns
+  end
+
+  def call(env)
+    res = @app.call(env)
+    path = env["PATH_INFO"]
+    @patterns.each do |pattern, data|
+      if path =~ pattern
+        res[1]["Cache-Control"] = data[:cache_control] if data.has_key?(:cache_control)
+        res[1]["Expires"] = (Time.now + data[:expires]).utc.rfc2822 if data.has_key?(:expires)
+        return res
+      end
+    end
+    res
+  end
+end
