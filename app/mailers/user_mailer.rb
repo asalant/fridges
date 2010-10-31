b3d7d18ec424df3a5a7161587c647cfeaa03d94a
Alon Salant <alon@salant.org>
2010-10-31 15:23:57 -0700

    CheckOutMyFridge.com is from address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "hello@checkoutmyfridge.com"
+  default :from => "CheckOutMyFridge.com <hello@checkoutmyfridge.com>"
   add_template_helper(FridgesHelper)
   default_url_options[:host] = 'frdg.us'
 
